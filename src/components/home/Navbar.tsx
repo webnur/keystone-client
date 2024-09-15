4cad5f2b3486a2f6fb1d18bdf3020c78d7a6adac
webnur <mdnuruli291@gmail.com>
2024-09-15 20:27:41 +0600

    header icons added

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,20 +1,38 @@
-import React from 'react'
-import logo from "/public/images/logo/keystone-logo.png"
-import Image from 'next/image'
-import Button from '../common/Button'
+import React from "react";
+import logo from "/public/images/logo/keystone-logo.png";
+import Image from "next/image";
+import Button from "../common/Button";
+import GlobIcon from "../icons/glob";
+import SearchIcon from "../icons/searchIcon";
+import LoveIcon from "../icons/LoveIcon";
+import CustomIcon from "../icons/CustomIcon";
 
 const Navbar = () => {
   return (
-    <div className='w-full md:w-[90%] mx-auto flex justify-between py-4'>
-        <div>
-           <Image className='w-full h-auto' src={logo} width={100} height={100} alt="logo" />
-        </div>
-        <div>
+    <div className="w-full md:w-[90%] mx-auto flex justify-between py-4">
+      <div>
+        <Image
+          className="w-full h-auto"
+          src={logo}
+          width={100}
+          height={100}
+          alt="logo"
+        />
+      </div>
+      <div>
+        <div className="flex items-center justify-around gap-2">
+          <GlobIcon />
+          <SearchIcon />
+          <LoveIcon />
+          <CustomIcon />
+          <div>
             <Button text="Sign in" />
             <Button text="Register" />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
