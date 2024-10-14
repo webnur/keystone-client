ba4adeeb34aef2c1255c7f920804ea1f26f21e5b
mdrayhanhowlader <rhowlader5314@gmail.com>
2024-10-14 15:08:49 +0600

    updated

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -195,7 +195,7 @@ export default function Home() {
 
   return (
     <div>
-      {/* <Navbar /> */}
+  
       <SubHeaderNav />
       <BannerWithDropdown
         backgroundImage={
@@ -209,7 +209,7 @@ export default function Home() {
       <WhereToStudy locations={mockLocations} />
       <ArticlesAndGuides articles={mockArticles} />
       <NewlyAddedPrograms programs={mockPrograms} />
-      {/* <Footer /> */}
+   
     </div>
   );
 }
