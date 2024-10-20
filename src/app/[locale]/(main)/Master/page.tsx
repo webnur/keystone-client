ecb3031584b27c7f50ef11d0f94570950f484174
webnur <mdnuruli291@gmail.com>
2024-10-20 07:36:31 +0600

    remove main file data

--- src/app/[locale]/(main)/Master/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MasterPage = () => {
-  return <div>MasterPage</div>;
-};
-
-export default MasterPage;
--- src/app/[locale]/(main)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className=" w-full bg-white">{children}</div>;
-};
-
-export default UserLayout;
