e1d75f2e99bfd373acf6acf4eaeea71004b4a10d
mdrayhanhowlader <rhowlader5314@gmail.com>
2024-10-21 23:06:17 +0600

    updated filter page saved program has been saved in localhost now

--- src/app/[locale]/(master)/master/discover-program/[id]/Filter.tsx
+++ src/app/[locale]/(master)/master/discover-program/[id]/Filter.tsx
@@ -18,13 +18,8 @@ const FilterComponent: React.FC = () => {
 
   // Predefined list of European countries, USA, and Canada
   const countryList = [
-<<<<<<< HEAD
     'USA', 'Canada', 'Germany', 'France', 'Italy', 'Spain', 'Netherlands',
     'Belgium', 'Switzerland', 'United Kingdom', 'Sweden', 'Norway',
-=======
-    'USA', 'Canada', 'Germany', 'France', 'Italy', 'Spain', 'Netherlands', 
-    'Belgium', 'Switzerland', 'United Kingdom', 'Sweden', 'Norway', 
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
     'Denmark', 'Finland', 'Poland', 'Portugal', 'Ireland', 'Austria', 'Greece'
   ];
 
@@ -63,11 +58,8 @@ const FilterComponent: React.FC = () => {
     setter((prevState) => !prevState);
   };
 
-<<<<<<< HEAD
   const isFilterSelected = (filter: string) => selectedFilters.includes(filter);
 
-=======
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
   return (
     <aside className="w-1/4 p-4 bg-gray-100">
       {/* Breadcrumb or Route Path */}
@@ -122,31 +114,23 @@ const FilterComponent: React.FC = () => {
           <ul className="space-y-2">
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('Artificial Intelligence')}
                   onChange={() => handleFilterToggle('Artificial Intelligence')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('Artificial Intelligence')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 Artificial Intelligence (272)
               </label>
             </li>
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('Automation')}
                   onChange={() => handleFilterToggle('Automation')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('Automation')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 Automation (153)
               </label>
             </li>
@@ -158,12 +142,7 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsLocationOpen)}>
           <h3 className="font-semibold mb-2">Location</h3>
-<<<<<<< HEAD
           <span>{isLocationOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isLocationOpen && (
           <>
@@ -190,31 +169,23 @@ const FilterComponent: React.FC = () => {
             <ul className="space-y-2">
               <li>
                 <label>
-<<<<<<< HEAD
                   <input
                     type="checkbox"
                     checked={isFilterSelected('USA')}
                     onChange={() => handleFilterToggle('USA')}
                     className="mr-2"
                   />
-=======
-                  <input type="checkbox" onChange={() => handleFilterToggle('USA')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                   USA (492)
                 </label>
               </li>
               <li>
                 <label>
-<<<<<<< HEAD
                   <input
                     type="checkbox"
                     checked={isFilterSelected('Germany')}
                     onChange={() => handleFilterToggle('Germany')}
                     className="mr-2"
                   />
-=======
-                  <input type="checkbox" onChange={() => handleFilterToggle('Germany')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                   Germany (329)
                 </label>
               </li>
@@ -227,42 +198,29 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsDegreeOpen)}>
           <h3 className="font-semibold mb-2">Degree Type</h3>
-<<<<<<< HEAD
           <span>{isDegreeOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isDegreeOpen && (
           <ul className="space-y-2">
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('MSc')}
                   onChange={() => handleFilterToggle('MSc')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('MSc')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 MSc (86)
               </label>
             </li>
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('MA')}
                   onChange={() => handleFilterToggle('MA')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('MA')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 MA (852)
               </label>
             </li>
@@ -274,42 +232,29 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsDurationOpen)}>
           <h3 className="font-semibold mb-2">Course Duration</h3>
-<<<<<<< HEAD
           <span>{isDurationOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isDurationOpen && (
           <ul className="space-y-2">
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('1 year')}
                   onChange={() => handleFilterToggle('1 year')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('1 year')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 1 year (498)
               </label>
             </li>
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('2 years')}
                   onChange={() => handleFilterToggle('2 years')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('2 years')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 2 years (446)
               </label>
             </li>
@@ -321,42 +266,29 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsPaceOpen)}>
           <h3 className="font-semibold mb-2">Study Pace</h3>
-<<<<<<< HEAD
           <span>{isPaceOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isPaceOpen && (
           <ul className="space-y-2">
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('Full-time')}
                   onChange={() => handleFilterToggle('Full-time')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('Full-time')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 Full-time (1085)
               </label>
             </li>
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('Part-time')}
                   onChange={() => handleFilterToggle('Part-time')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('Part-time')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 Part-time (447)
               </label>
             </li>
@@ -368,42 +300,29 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsLanguageOpen)}>
           <h3 className="font-semibold mb-2">Language</h3>
-<<<<<<< HEAD
           <span>{isLanguageOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isLanguageOpen && (
           <ul className="space-y-2">
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('English')}
                   onChange={() => handleFilterToggle('English')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('English')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 English (1400)
               </label>
             </li>
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('Spanish')}
                   onChange={() => handleFilterToggle('Spanish')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('Spanish')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 Spanish (95)
               </label>
             </li>
@@ -415,42 +334,29 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsFormatOpen)}>
           <h3 className="font-semibold mb-2">Study Format</h3>
-<<<<<<< HEAD
           <span>{isFormatOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isFormatOpen && (
           <ul className="space-y-2">
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('On-Campus')}
                   onChange={() => handleFilterToggle('On-Campus')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('On-Campus')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 On-Campus (1293)
               </label>
             </li>
             <li>
               <label>
-<<<<<<< HEAD
                 <input
                   type="checkbox"
                   checked={isFilterSelected('Distance Learning')}
                   onChange={() => handleFilterToggle('Distance Learning')}
                   className="mr-2"
                 />
-=======
-                <input type="checkbox" onChange={() => handleFilterToggle('Distance Learning')} className="mr-2" />
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
                 Distance Learning (273)
               </label>
             </li>
@@ -462,12 +368,7 @@ const FilterComponent: React.FC = () => {
       <div className="mb-4">
         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleAccordion(setIsDeadlineOpen)}>
           <h3 className="font-semibold mb-2">Application Deadline</h3>
-<<<<<<< HEAD
           <span>{isDeadlineOpen ? <IoClose /> : <FaArrowDown />}</span>
-=======
-          <span>{isFieldsOpen ? <IoClose /> : <FaArrowDown />}</span>
-
->>>>>>> e19162b (updated filter page saved program has been saved in localhost now)
         </div>
         {isDeadlineOpen && (
           <ul className="space-y-2">
