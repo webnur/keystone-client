c71cd0b5bffcc94be22930a4c10c50072857dbf3
webnur <mdnuruli291@gmail.com>
2024-10-28 23:36:24 +0600

    set and get local storage done

--- src/components/common/PopularFields.tsx
+++ src/components/common/PopularFields.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { subjectsData } from "./MockData";
+import { setToLocalStorage } from "@/utils/local-storage";
+import Link from "next/link";
 
 const PopularFields: React.FC = () => {
+  const handleFieldClick = (subjectName: string, fields: string | string[]) => {
+    const data = JSON.stringify({ subject: subjectName, fields });
+    setToLocalStorage("selectedField", data);
+  };
+
   return (
     <section className="py-16 bg-gray-900 text-white w-full mx-auto">
       <h2 className="text-3xl font-bold mb-8 text-left w-11/12 mx-auto">
@@ -15,16 +22,22 @@ const PopularFields: React.FC = () => {
             </h3>
             <div className="flex flex-wrap gap-2">
               {subject.fields.map((field, i) => (
-                <button
+                <Link
                   key={i}
-                  className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-full transition-colors duration-300"
+                  href="/master/discover-program/computer-science"
+                  onClick={() => handleFieldClick(subject.name, field)}
+                  className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-full transition-colors duration-300 inline-block"
                 >
                   {field}
-                </button>
+                </Link>
               ))}
-              <button className="text-red-600 hover:text-red-700 border border-red-600 hover:border-red-700 py-2 px-4 rounded-full transition-colors duration-300">
+              <Link
+                href="/master/discover-program/computer-science"
+                onClick={() => handleFieldClick(subject.name, subject.fields)} // Pass all fields to handleFieldClick
+                className="text-red-600 hover:text-red-700 border border-red-600 hover:border-red-700 py-2 px-4 rounded-full transition-colors duration-300 inline-block"
+              >
                 See all in {subject.name} →
-              </button>
+              </Link>
             </div>
           </div>
         ))}
